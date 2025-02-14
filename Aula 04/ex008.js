6c4a863dffc18c0923ccbef24784cf5867baf893
João Gabriel <cris27negrao@gmail.com>
2025-02-14 16:50:51 -0300

    switch

---
+++ Aula 04/ex008.js
@@ -0,0 +1,9 @@
+var vel = 70
+console.log(`A sua velocidade é ${vel} km/h`)
+
+    if(vel > 60){
+        console.log(`Voce ultrapaçou a velocidade permitida`)
+    }
+    else{
+        console.log(`Dirija sempre com cinto de segurança`)
+    }
