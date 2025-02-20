e915a855eb05841dde37b2e40628d73683e84eec
João Gabriel <cris27negrao@gmail.com>
2025-02-20 15:50:04 -0300

    modulo D concluido

--- Aula 07/script.js
+++ Aula 07/script.js
@@ -8,6 +8,42 @@ function verificar(){
     }else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = 'Homen'
+                if(idade >=0 && idade <10){
+                    //criança
+                    img.setAttribute('src', 'menino.jpg')
+                }else if(idade < 21){
+                    //jovem
+                    img.setAttribute('src', 'jovemH.jpg')
+                }else if(idade <50){
+                    //adulto
+                    img.setAttribute('src', 'homen.jpg')
+                }else{
+                    //idoso
+                    img.setAttribute('src', 'idoso.jpg')
+                }
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+                if(idade >=0 && idade <10){
+                    //criança
+                    img.setAttribute('src', 'menina.jpg')
+                }else if(idade < 21){
+                    //jovem
+                    img.setAttribute('src', 'jovemM.jpg')
+                }else if(idade <50){
+                    //adulto
+                    img.setAttribute('src', 'mulher.jpg')
+                }else{
+                    //idoso
+                    img.setAttribute('src', 'idosa.jpg')
+                }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
